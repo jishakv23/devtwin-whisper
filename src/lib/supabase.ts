e47d9d4ce6272c764abaf23e930ee5ed13aec812
Jisha Varghese <jisha.v@keyvalue.systems>
2025-09-26 01:51:15 +0530

    Add supabase url

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL! ||
+  'https://dxpkkikicfxpzxzyygqq.supabase.co';
+const supabaseKey =
+  import.meta.env.VITE_SUPABASE_ANON_KEY! ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4cGtraWtpY2Z4cHp4enl5Z3FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MzQ3MzQsImV4cCI6MjA3NDMxMDczNH0.xBauHy1NzPupwDRthJWDOXk07GdnzyPNC8M6lTjedy8';
 
 console.log(supabaseUrl, supabaseKey);
 export const supabase = createClient(supabaseUrl, supabaseKey, {
